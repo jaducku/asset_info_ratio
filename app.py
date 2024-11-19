334550afd81336dea5d5f4f4159c3b0a12f9b1a3
김성지 <jaducku@gmail.com>
2024-11-19 04:46:52 +0000

    exchange rate 추가

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import os
 from supabase import create_client, Client
 from dotenv import load_dotenv
 
-gold_price_per_ozt=''
-silver_price_per_ozt=''
-won_dollar_ratio=''
+dollar_index=0
+won_dollar_rate=0
+won_yen_rate=0
+won_yuan_rate=0
+dollar_gold_rate=0
+dollar_silver_rate=0
+dollar_bit_rate=0
+p2p_ratio: 1.07
+
 
 url = 'https://finance.naver.com/marketindex/'
 
@@ -17,7 +23,7 @@ if response.status_code == 200:  # 정상 응답 반환 시 아래 코드블록 
     soup = BeautifulSoup(response.text, 'html.parser')  # 응답 받은 HTML 파싱
     exchange_list = soup.find('div',id='sInput')  # 파싱한 데이터에서 div 태그 내 news_list 클래스 내 데이터 저장
     usd = exchange_list.find('option',class_='selectbox-default')
-    won_dollar_ratio = usd['value']
+    won_dollar_rate = usd['value']
 else:
     print('error')  # 오류 시 메시지 출력
 
@@ -29,7 +35,7 @@ if response.status_code == 200:  # 정상 응답 반환 시 아래 코드블록 
     material_list = soup.find('script',id='__NEXT_DATA__').text
     json_data = json.loads(material_list)
     internal_gold_price_ozt = json_data['props']['pageProps']['dehydratedState']['queries'][1]['state']['data']['GetMetalQuoteV3']['results'][0]['ask']
-    gold_price_per_ozt = internal_gold_price_ozt
+    dollar_gold_rate = internal_gold_price_ozt
 else:
     print('error')  # 오류 시 메시지 출력
 
@@ -41,7 +47,7 @@ if response.status_code == 200:  # 정상 응답 반환 시 아래 코드블록 
     material_list = soup.find('script',id='__NEXT_DATA__').text
     json_data = json.loads(material_list)
     internal_silver_price_ozt = json_data['props']['pageProps']['dehydratedState']['queries'][1]['state']['data']['GetMetalQuoteV3']['results'][0]['ask']
-    silver_price_per_ozt = internal_silver_price_ozt
+    dollar_silver_rate = internal_silver_price_ozt
 else:
     print('error')  # 오류 시 메시지 출력
 
@@ -56,15 +62,19 @@ supabase: Client = create_client(supabase_url, supabase_key)
 
 # 삽입할 데이터
 data = {
-    "gold_price_per_ozt": gold_price_per_ozt,
-    "silver_price_per_ozt": silver_price_per_ozt,
-    "won_dollar_ratio": won_dollar_ratio,
+    "dollar_index": dollar_index,
+    "won_dollar_rate": won_dollar_rate,
+    "won_yen_rate": won_yen_rate,
+    "won_yuan_rate": won_yuan_rate,
+    "dollar_gold_rate": dollar_gold_rate,
+    "dollar_silver_rate": dollar_silver_rate,
+    "dollar_bit_rate": dollar_bit_rate,
     "p2p_ratio": 1.07
 }
 
 # 데이터 삽입
 try:
-    response = supabase.schema('assets').table("aurea_info").insert(data).execute()
+    response = supabase.schema('assets').table("exchange_rate").insert(data).execute()
     print(response)
 except Exception as e:
     print("오류 발생:", e)
